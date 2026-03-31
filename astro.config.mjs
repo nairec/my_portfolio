f429cccde648d55e6be37935f8fc64b5ebae0a7f
nairec <irecgc@gmail.com>
2026-03-31 19:34:50 +0200

    changed dev node version to v22

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 import react from "@astrojs/react";
 
@@ -10,6 +11,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   output: "server",
-
+  adapter: vercel(),
   integrations: [react()],
 });
